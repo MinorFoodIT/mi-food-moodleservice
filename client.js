369c1870a39640ae6ce2f29e7a6cfc0b5dacdf1a
akarat <akrs33@gmail.com>
2019-02-06 17:50:02 +0700

    Insert course and course categories

--- client.js
+++ client.js
@@ -42,6 +42,14 @@ pools.then( pool => {
 );
 */
 
+function truncate_table(pool , tablename) {
+    return pool.request()
+        .query('truncate table '+tablename+'')
+        .then(result =>{
+            console.log('truncate '+tablename+' complete')
+        })
+}
+
 function insertTable_CourseCategories(course ,pool){
 
     return pool.request()
@@ -67,35 +75,59 @@ function insertTable_CourseCategories(course ,pool){
         })
 }
 
+function insertTable_Courses(course ,pool){
+
+    return pool.request()
+        .input('id', sql.Int, course.id)
+        .input('shortname', sql.NVarChar(255), course.shortname)
+        .input('categoryid', sql.Int, course.categoryid)
+        .input('categorysortorder', sql.Int, course.categorysortorder)
+        .input('fullname', sql.NVarChar(255), course.fullname)
+        .input('displayname', sql.NVarChar(255), course.displayname)
+        .input('idnumber', sql.NVarChar(255), course.idnumber)
+        .input('startdate', sql.Int, course.startdate)
+        .input('enddate', sql.Int, course.enddate)
+        .input('visible', sql.Int, course.visible)
+        .input('enablecompletion', sql.Int, course.enablecompletion)
+        .input('completionnotify', sql.Int, course.completionnotify)
+
+        .query('Insert into xx_Moodle_Courses (id,shortname,categoryid,categorysortorder,fullname,displayname,idnumber,startdate,enddate,visible,enablecompletion,completionnotify) ' +
+            'values (@id,@shortname,@categoryid,@categorysortorder,@fullname,@displayname,@idnumber,@startdate,@enddate,@visible,@enablecompletion,@completionnotify)')
+        .then(result => {
+            console.dir(result)
+        })
+}
 sql.connect(config).then(pool => {
 
     axios.get('/webservice/rest/server.php?wstoken=' + config.token + '&wsfunction=core_course_get_categories&moodlewsrestformat=json&criteria[0][key]=parent&criteria[0][value]=0')
         .then(function (response) {
+            truncate_table(pool,'xx_Moodle_CourseCategories')
             var course_data = response.data;
             for (const course of course_data) {
-
-                console.log(course.id);
                 insertTable_CourseCategories(course,pool)
-                /*
-                pool.request()
-                    .input('id', sql.Int, course.id)
-                    .query('Insert into xx_moodle_core_course_get_categories (id) values (@id)')
-                    .then(result => {
-                        console.dir(result)
-                    })
-                */
-
             }
-
         })
         .catch(function (error) {
-            // handle error
-            console.log(error);
+            console.log('API : Moodle : core_course_get_categories : error '+error);
         })
         .then(function () {
             // always executed
         });
 
+    axios.get('/webservice/rest/server.php?wstoken=' + config.token + '&wsfunction=core_course_get_course&moodlewsrestformat=json')
+        .then(function (response) {
+            truncate_table(pool,'xx_Moodle_Courses')
+            var course_data = response.data;
+            for (const course of course_data) {
+                insertTable_Courses(course,pool)
+            }
+        })
+        .catch(function (error) {
+            console.log('API : Moodle : core_courses : error '+error);
+        })
+        .then(function () {
+            // always executed
+        });
 }).catch(err =>{
     console.info('SQL Connect error '+err)
 })
